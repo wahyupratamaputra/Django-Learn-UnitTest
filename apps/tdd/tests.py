ff7f14e4d28f187459a3f605f45c413cc41a0176
Wahyu <Wahyu@My-MacBook-Pro.local>
2019-03-08 12:12:23 +0800

    functional for selenium input in browser

--- apps/tdd/tests.py
+++ apps/tdd/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from .views import index
+from django.http import HttpRequest
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -11,3 +12,11 @@ class HomePageTest(TestCase):
 
     # def test_bad_math(self):
     #     self.assertEqual(1+1, 3)
+
+    def test_homepage_return_correct_html(self):
+        request = HttpRequest
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>To-do list</title>', html)
+        self.assertTrue(html.endswith('</html>'))
